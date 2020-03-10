718d54b4ab636806023437eeda33efe9bd5a6518
RAMESH KRISHNAN BALASUBRAMANI <51070088+smushball27295@users.noreply.github.com>
2020-03-09 23:15:26 -0400

    Add files via upload

---
+++ sentimental analysis1.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  9 23:06:14 2020
+
+@author: ramesh
+"""
+from textblob import TextBlob 
+#file 1
+errors = []                       # The list where we will store results.
+linenum = 0
+substr = "Name"         # Substring to search for.
+with open ('webscrape_1.txt', 'rt') as myfile:
+    for line in myfile:
+        linenum += 1
+        if line.find(substr) != -1:    # if case-insensitive match,
+            errors.append(line.rstrip('\n'))
+for err in errors:
+    print(err) 
+    
+linenum = 0
+substr = "Purpose"         # Substring to search for.
+with open ('webscrape_1.txt', 'rt') as myfile:
+    for line in myfile:
+        linenum += 1
+        if line.find(substr) != -1:    # if case-insensitive match,
+            errors.append(line.rstrip('\n'))
+for err in errors:
+    print(err)  
+
+#file4
+linenum = 0
+substr = "Purpose"         # Substring to search for.
+with open ('webscrape_4.txt', 'rt') as myfile:
+    for line in myfile:
+        linenum += 1
+        if line.find(substr) != -1:    # if case-insensitive match,
+            errors.append(line.rstrip('\n'))
+for err in errors:
+    print(err)   
+
+                      # The list where we will store results.
+linenum = 0
+substr = "Name"         # Substring to search for.
+with open ('webscrape_4.txt', 'rt') as myfile:
+    for line in myfile:
+        linenum += 1
+        if line.find(substr) != -1:    # if case-insensitive match,
+            errors.append(line.rstrip('\n'))
+for err in errors:
+    print(err)       
+    
+    
+#file8
+linenum = 0
+substr = "Purpose"         # Substring to search for.
+with open ('webscrape_8.txt', 'rt') as myfile:
+    for line in myfile:
+        linenum += 1
+        if line.find(substr) != -1:    # if case-insensitive match,
+            errors.append(line.rstrip('\n'))
+for err in errors:
+    print(err)       
+
+
+errors = []                       # The list where we will store results.
+linenum = 0
+substr = "Name"         # Substring to search for.
+with open ('webscrape_1.txt', 'rt') as myfile:
+    for line in myfile:
+        linenum += 1
+        if line.find(substr) != -1:    # if case-insensitive match,
+            errors.append(line.rstrip('\n'))
+for err in errors:
+    print(err)   
+
+
+
+x=[]
+for i in h:
+    blob = TextBlob(str(i))
+    x.append(blob.sentiment)
+
+i = 0
+while i<len(errors):
+    if errors[i][1]=='N' or errors[i][0]=='N':
+        errors.remove(errors[i])
+    i+=1
+
+while i<len(errors):
+    if errors[i][1]=='N':
+        errors.remove(errors[i])
+    i+=1
+    
+    
+for i in range(len(x)):
+    c.append(float(x[i].polarity))
+
+len(c)
+
+
+def Convert(lst): 
+    res_dct = {lst[i]: i for i in range(0, len(lst))} 
+    return res_dct    
+
+s = Convert(errors) 
+u = Convert(c)
+for i in u.keys():
+    r.append(i)
+    
+    
+r.sort()
+len(r)
+for i in r:
+    print(u[i])    
